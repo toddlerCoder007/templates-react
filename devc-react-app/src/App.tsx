84857e3af6e9eba000a9edb5548cbf471144082d
toddlerCoder007 <167171420+toddlerCoder007@users.noreply.github.com>
2025-03-10 03:10:50 +0000

    working heatmap polygon

---
+++ devc-react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Base from '.'
+
+function App() {
+
+  return (
+    <Base />
+  )
+}
+
+export default App
---
+++ devc-react-app/src/index.tsx
@@ -0,0 +1,15 @@
+import HeatmapMapPolygon from './components/Base/heatmap_polygon';
+
+export default function Base() {
+    const countryColors: { [country: string]: [number, number, number, number] } = {
+        "Singapore": [0, 0, 0, 0.5],  // 🔴 Red
+        "Malaysia": [0, 0, 255, 0.5],  // 🔵 Blue
+        "Indonesia": [0, 255, 0, 0.5],  // 🟢 Green
+        "Thailand": [255, 165, 0, 0.5],  // 🟠 Orange
+        "Philippines": [75, 0, 130, 0.5]  // 🟣 Purple
+    };
+
+    return (
+        <HeatmapMapPolygon countryColors={countryColors}/>
+    )
+}
