387e3c3c2a3f1115c2d43883deeb1c53503c32dd
Tay Li Na <lina.tay96@gmail.com>
2025-04-13 21:06:51 +0800

    feat: checkpoint

--- devc-react-app/src/index.tsx
+++ devc-react-app/src/index.tsx
@@ -1,11 +1,39 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, IconButton, Toolbar } from '@mui/material';
+import { useState } from 'react';
 import HeatmapMap from './components/Base/heatmap';
 import ContextFilter from './components/ContextFilter/ContextFilterDrawer';
 
 export default function Base() {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    // Toggle drawer visibility
+    const toggleDrawer = () => {
+        setIsDrawerOpen(prevState => !prevState);
+    };
+
     return (
         <>
-            <HeatmapMap />
-            <ContextFilter isOpen={true} onClose={() => { }} />
+            {/* MUI AppBar with Toolbar */}
+            <AppBar position="sticky">
+                <Toolbar>
+                    {/* Hamburger Menu Button inside the Toolbar */}
+                    <IconButton edge="start" color="inherit" onClick={toggleDrawer}>
+                        <MenuIcon /> {/* MUI Hamburger Icon */}
+                    </IconButton>
+
+                    {/* Title */}
+                    <h1 style={{ marginLeft: '16px' }}>Heatmap Dashboard</h1>
+                </Toolbar>
+            </AppBar>
+
+            {/* Map Container */}
+            <div className="heatmap-map-container" style={{ height: 'calc(100vh - 64px)' }}> {/* Adjust for AppBar height */}
+                <HeatmapMap />
+            </div>
+
+            {/* Context Filter Drawer */}
+            <ContextFilter isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
         </>
     );
 }
